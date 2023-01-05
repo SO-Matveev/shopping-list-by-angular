f0859594e30ce4af5c1993d9ae3b7a3f5f4ad245
stas-notebook <matveev.stanislav@yahoo.com>
2023-01-05 22:58:36 +0300

    add recipe- detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'A test Recipe',
+      'Italian Carbonara',
       'This is simply a test',
       'https://i.pinimg.com/originals/ca/00/21/ca00210210b7f8ff28195f6a32a558e5.jpg'
+    ),
+    new Recipe(
+      'Beef Steak',
+      'This is simply a test',
+      'https://catchsuccess.ru/wp-content/uploads/0/f/c/0fcc77f2baf7d98bda2eeaf433c5fd68.jpeg'
     ),
   ];
   constructor() {}
