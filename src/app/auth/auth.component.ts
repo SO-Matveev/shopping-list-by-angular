2f823583a2c089bd08a01b857bb23edec9171cb0
s.matveev <matveev.stanislav@yahoo.com>
2024-07-07 17:11:48 +0300

    add Login Effect(ngrx@effects)

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,31 +1,46 @@
-import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { AuthService, AuthResponseData } from "./auth.service";
 import { Observable, Subscription } from "rxjs";
 import { Router } from "@angular/router";
 import { AlertComponent } from "../shared/alert/alert.component";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
+import { Store } from "@ngrx/store";
+import * as fromApp from "../store/app.reducer";
+import * as AuthActions  from "./store/auth.actions";
+import { authState } from "@angular/fire/auth";
+
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnDestroy {
-  @ViewChild(PlaceholderDirective)
+export class AuthComponent implements OnInit, OnDestroy {
+  @ViewChild(PlaceholderDirective, { static: false })
   alertHost: PlaceholderDirective;
 
   isLoginMode: boolean = true
   isLoading: boolean = false
-  error: string | null = null
+  error: string = null
 
   private closeSub: Subscription;
 
   constructor(
     private authService: AuthService,
     private router: Router,
-    private componentFactoryResolver: ComponentFactoryResolver
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private store: Store<fromApp.AppState>,
   ) {
+  }
+  ngOnInit() {
+    this.store.select('auth').subscribe(authState => {
+      this.isLoginMode = authState.loading;
+      this.error = authState.authError
+      if (this.error) {
+        this.showErrorAlert(this.error)
+      }
+    })
   }
 
   onSwitchMode(): void {
@@ -43,21 +58,26 @@ export class AuthComponent implements OnDestroy {
 
     this.isLoading = true;
     if (this.isLoginMode) {
-      authObs = this.authService.login(email, password)
+      // authObs = this.authService.login(email, password)
+      this.store.dispatch(new AuthActions.LoginStart({
+        email: email,
+        password: password
+      }))
     } else {
       authObs = this.authService.signup(email, password)
     }
-    authObs.subscribe(
-      resData => {
-        this.isLoading = false
-        this.router.navigate(['/recipes'])
-      },
-      errorMessage => {
-        this.error = errorMessage;
-        this.showErrorAlert(errorMessage)
-        this.isLoading = false
-      }
-    );
+
+    // authObs.subscribe(
+    //   resData => {
+    //     this.isLoading = false
+    //     this.router.navigate(['/recipes'])
+    //   },
+    //   errorMessage => {
+    //     this.error = errorMessage;
+    //     this.showErrorAlert(errorMessage)
+    //     this.isLoading = false
+    //   }
+    // );
     form.reset()
   }
 
--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,6 +1,8 @@
 import { Action } from "@ngrx/store";
 
-export const LOGIN = 'LOGIN'
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN = 'LOGIN';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT'
 
 export class Login implements Action {
@@ -21,4 +23,24 @@ export class Logout implements Action {
   readonly type = LOGOUT;
 }
 
-export type AuthActionsType = Login | Logout
+export class LoginStart implements Action {
+  readonly type = LOGIN_START
+
+  constructor(
+    public payload: {
+      email: string;
+      password: string;
+    }
+  ) {}
+}
+
+export class LoginFail implements Action {
+  readonly type = LOGIN_FAIL;
+  constructor(
+    public payload: string
+  ) {
+
+  }
+}
+
+export type AuthActionsType = Login | Logout | LoginStart | LoginFail
